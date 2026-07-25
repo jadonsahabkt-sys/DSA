91d1ccadb851aa66bb1d59067a1466f26b78e6c7
Karan Thakur <jadonsahabkt@gmail.com>
2026-07-25 21:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (12.53%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,17 +2,11 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         for(int i=0;i<=30;i++){
-            if(pow(2,i)==n){
-                return true;
-            }
-
-
-
+           if(pow(2,i)==n){
+            return true;
+           }
         }
-   
-            return false;
-
-     }
+        return false;
         
-
+    }
 };
